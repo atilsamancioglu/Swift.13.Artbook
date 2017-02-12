45153ae0b822675d444c4f032f615167b0484c8b
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:20:26 +0300

    final version

--- Art Book/ViewController.swift
+++ Art Book/ViewController.swift
@@ -7,19 +7,129 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var nameArray = [String]()
+    var yearArray = [Int]()
+    var artistArray = [String]()
+    var imageArray = [UIImage]()
+    
+    var selectedPainting = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        retrieveInfo()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.retrieveInfo), name: NSNotification.Name(rawValue:"paintingCreated"), object: nil)
+        
     }
+    
+    func retrieveInfo(){
+        
+        self.nameArray.removeAll(keepingCapacity: false)
+        self.yearArray.removeAll(keepingCapacity: false)
+        self.artistArray.removeAll(keepingCapacity: false)
+        self.imageArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        
+        do {
+            
+            let results = try context.fetch(fetchRequest)
+            
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    
+                    if let year = result.value(forKey: "year") as? Int {
+                        self.yearArray.append(year)
+                    }
+                    
+                    if let artist = result.value(forKey: "artist") as? String {
+                        self.artistArray.append(artist)
+                    }
+                    
+                    if let imageData = result.value(forKey: "image") as? Data {
+                        let image = UIImage(data: imageData)
+                        self.imageArray.append(image!)
+                        
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+                
+                
+            }
+            
+            
+            
+        } catch {
+            print("there was an error")
+        }
+        
+        
+        
+    }
+    
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.selectedPainting = nameArray[indexPath.row]
+        performSegue(withIdentifier: "toCreateVC", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "toCreateVC" {
+            let destinationVC = segue.destination as! createVC
+            destinationVC.chosenPainting = selectedPainting
+        }
+        
+    }
+    
+    @IBAction func addButtonClicked(_ sender: Any) {
+        
+        self.selectedPainting = ""
+        performSegue(withIdentifier: "toCreateVC", sender: nil)
+        
+    }
+    
 
 }
 
